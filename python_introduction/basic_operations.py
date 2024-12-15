2424f054af6cc0604aa65c75ed38fbb65e5a825c
meta-avi <95247924+meta-avi@users.noreply.github.com>
2024-12-15 20:17:10 +0100

    commit murder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5 
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+
+print(Addition)
+print(Subtraction)
+print(Multiplication)
