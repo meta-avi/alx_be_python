4bc98bd9640921f50c635caee1e2c7ad5649e1f3
meta-avi <95247924+meta-avi@users.noreply.github.com>
2024-12-16 02:44:47 +0100

    commit Calculate Income...

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,4 +1,4 @@
-monthly_income = int(input("Enter your monthly income"))
+monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses"))
 
 Monthly_Savings = monthly_income - monthly_expenses
